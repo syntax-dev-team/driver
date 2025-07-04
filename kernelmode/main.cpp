6b665ba2b07508e42b3bfda7959bd401a7108c02
Anh Tran <antsoupx@gmail.com>
2025-07-04 23:54:52 +1000

    added to kerneldriver

--- kernelmode/main.cpp
+++ kernelmode/main.cpp
@@ -1,8 +1,43 @@
 #include <ntifs.h>
 extern "C" {
-	NTKERNELAPI
+	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName,
+		PDRIVER_INITIALIZE InitializationFunction);
+
+	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAddress,
+		PEPROCESS TargetProcess, PVOID TargetAddress,
+		SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode,
+		PSIZE_T ReturnSize);
+}
+
+void D_DbgPrint(PCSTR text) {
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
+}
+
+namespace driver {
+	namespace codes {
+		constexpr ULONG attach = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG read = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	}
+	struct Request {
+		HANDLE process_id;
+
+		PVOID target;
+		PVOID buffer;
+
+		SIZE_T size;
+		SIZE_T returnsize;
+	};
+}
+
+NTSTATUS main(PDRIVER_OBJECT, PUNICODE_STRING registry_path) {
+	return STATUS_SUCCESS;
 }
 
 NTSTATUS DriverEntry() {
+	D_DbgPrint("Hello World!");
+
+	UNICODE_STRING driver_name = {};
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\BigBallsDriver");
 	return STATUS_SUCCESS;
 }

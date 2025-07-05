ea40b865478079960d1ea3c99d59095869a1ecd1
Anh Tran <antsoupx@gmail.com>
2025-07-05 14:33:15 +1000

    driver initiasation

--- kernelmode/main.cpp
+++ kernelmode/main.cpp
@@ -30,14 +30,47 @@ namespace driver {
 	};
 }
 
-NTSTATUS main(PDRIVER_OBJECT, PUNICODE_STRING registry_path) {
+NTSTATUS main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) { // Real entry
+	UNREFERENCED_PARAMETER(registry_path);
+
+	UNICODE_STRING device_name = {};
+	RtlInitUnicodeString(&device_name, L"\\Device\\BigBallsDriver");
+	// Create a device object
+	PDEVICE_OBJECT device_object = nullptr;
+	NTSTATUS status = IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
+
+	if (status != STATUS_SUCCESS) {
+		D_DbgPrint("[FAILED TO CREATE DRIVER]\n");
+		return status;
+	}
+	D_DbgPrint("[DEVICE OBJECT CREATED SUCCESFULLY]\n"); 
+
+	UNICODE_STRING symbolic_link = {};
+	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\BigBallsDriver");
+	status = IoCreateSymbolicLink(&symbolic_link, &device_name);
+
+	if (status != STATUS_SUCCESS) {
+		D_DbgPrint("[FAILED TO CREATE SYMBOLIC LINK]\n");
+		return status;
+	}
+	D_DbgPrint("[SYMBOLIC LINK ESTABLISHED SUCCESFULLY]\n");
+
+	SetFlag(device_object->Flags, DO_BUFFERED_IO);
+
+
+
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS DriverEntry() {
-	D_DbgPrint("Hello World!");
 
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) { // Driver Entry for mappper (parameters are unsued)
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	D_DbgPrint("[SYNTAX DRIVER RUNNING]\n");
+
+	// Create a proper driver and specify real entrypoint
 	UNICODE_STRING driver_name = {};
 	RtlInitUnicodeString(&driver_name, L"\\Driver\\BigBallsDriver");
-	return STATUS_SUCCESS;
+	return IoCreateDriver(&driver_name,&main);
 }
